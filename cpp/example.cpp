865c72ab092f68dc4608e5103aaf75aafb54342f
Enes Ozturk <enesozturk.d@gmail.com>
2021-08-30 16:04:11 +0300

    refactor: update module name

--- cpp/example.cpp
+++ cpp/example.cpp
@@ -12,7 +12,7 @@ void installModule(jsi::Runtime &jsiRuntime)
 	// jsi::Function::createFromHostFunction will create a JavaScript function based on a "host" (read C++) function
 	auto multiply = jsi::Function::createFromHostFunction(
 		jsiRuntime,										   // JSI runtime instance
-		jsi::PropNameID::forAscii(jsiRuntime, "multiply"), // Internal function name
+		jsi::PropNameID::forAscii(jsiRuntime, "getResult"), // Internal function name
 		1,												   // Number of arguments in function
 		// This is a C++ lambda function, the empty [] at the beginning is used to capture pointer/references so that they don't get de-allocated
 		// Then you get another instance of the runtime to use inside the function, a "this" value from the javascript world, a pointer to the arguments (you can treat it as an array) and finally a count for the number of arguments
@@ -30,7 +30,7 @@ void installModule(jsi::Runtime &jsiRuntime)
 		});
 
 	// Registers the function on the global object
-	jsiRuntime.global().setProperty(jsiRuntime, "multiply", std::move(multiply));
+	jsiRuntime.global().setProperty(jsiRuntime, "getResult", std::move(multiply));
 }
 
 void cleanUpModule()
